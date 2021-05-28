798651cfabdddeff76b8b14bd3695a719107bfb4
Edgar <edgardo951.ef@gmail.com>
2021-05-28 01:23:38 -0300

    creación del modelo Personaje y la migration del mismo

---
+++ migrations/20210528042143-create-personaje.js
@@ -0,0 +1,39 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Personajes', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      imagen: {
+        type: Sequelize.BLOB
+      },
+      nombre: {
+        type: Sequelize.STRING
+      },
+      edad: {
+        type: Sequelize.INTEGER
+      },
+      peso: {
+        type: Sequelize.FLOAT
+      },
+      historia: {
+        type: Sequelize.TEXT
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Personajes');
+  }
+};
---
+++ models/personaje.js
@@ -0,0 +1,27 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Personaje extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  Personaje.init({
+    imagen: DataTypes.BLOB,
+    nombre: DataTypes.STRING,
+    edad: DataTypes.INTEGER,
+    peso: DataTypes.FLOAT,
+    historia: DataTypes.TEXT
+  }, {
+    sequelize,
+    modelName: 'Personaje',
+  });
+  return Personaje;
+};
